8981c0b139d7a17ad9228e66f0d1d53ba20dc0f2
Kos <kos@scompiler.ru>
2019-10-13 05:17:16 +0300

    unused service and component removed

--- projects/ngx-red-zoom/src/lib/ngx-red-zoom.module.ts
+++ projects/ngx-red-zoom/src/lib/ngx-red-zoom.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { NgxRedZoomComponent } from './ngx-red-zoom.component';
 import { NgxRedZoomDirective } from './ngx-red-zoom.directive';
 
 
-
 @NgModule({
-  declarations: [NgxRedZoomComponent, NgxRedZoomDirective],
-  imports: [
-  ],
-  exports: [NgxRedZoomComponent, NgxRedZoomDirective]
+    declarations: [
+        NgxRedZoomDirective
+    ],
+    exports: [
+        NgxRedZoomDirective
+    ]
 })
 export class NgxRedZoomModule { }
--- projects/ngx-red-zoom/src/public-api.ts
+++ projects/ngx-red-zoom/src/public-api.ts
@@ -2,7 +2,4 @@
  * Public API Surface of ngx-red-zoom
  */
 
-export * from './lib/ngx-red-zoom.service';
-export * from './lib/ngx-red-zoom.component';
 export * from './lib/ngx-red-zoom.module';
-// export * from './lib/ngx-red-zoom.directive';
