9fa85ac6fb6c2291e17f559ce85d6999ac8e6fac
Kos <kos@scompiler.ru>
2019-10-14 13:10:32 +0300

    bump version 0.1.0

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+
+
+function copyLibrary() {
+    return gulp.src('dist/ngx-red-zoom/**/*')
+        .pipe(gulp.dest('package'));
+}
+
+
+function copyStyles() {
+    return gulp.src('src/scss/styles/**/*')
+        .pipe(gulp.dest('package/scss/styles'));
+}
+
+
+function copyScss() {
+    return gulp.src('src/scss/base.scss')
+        .pipe(gulp.dest('package/scss'));
+}
+
+
+exports.pack = gulp.series(copyLibrary, copyStyles, copyScss);
+
