3469d0458a43d39d504452db6ffa9f5075562fcc
Kos <kostyaveselovsky@gmail.com>
2021-05-24 08:20:06 +0300

    Fixed: Error in the test of the RedZoomDirective

--- projects/ngx-red-zoom/src/lib/red-zoom.directive.spec.ts
+++ projects/ngx-red-zoom/src/lib/red-zoom.directive.spec.ts
@@ -2,7 +2,8 @@ import { RedZoomDirective } from './red-zoom.directive';
 
 describe('RedZoomDirective', () => {
     it('should create an instance', () => {
-        const directive = new RedZoomDirective(null, null, null, null, null);
+        // @ts-ignore
+        const directive = new RedZoomDirective(null, null, null, null);
         expect(directive).toBeTruthy();
     });
 });
