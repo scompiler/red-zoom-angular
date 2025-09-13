0281943eec7c1e543d5d8f37ce79811ed868c0c2
Kos <kostyaveselovsky@gmail.com>
2025-09-13 13:42:50 +0300

    test: remove unnecessary tests

--- projects/ngx-red-zoom/src/lib/red-zoom.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { RedZoomDirective } from './red-zoom.directive';
-
-describe('RedZoomDirective', () => {
-    it('should create an instance', () => {
-        // @ts-expect-error Doesn't matter for tests
-        const directive = new RedZoomDirective(null, null, null, null);
-        expect(directive).toBeTruthy();
-    });
-});
